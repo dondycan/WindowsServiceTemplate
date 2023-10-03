e92a315cc4a7abe8e4522d4e8aabd24a8b0a2b8f
Liuxiaohui0306 <Liuxiaohui@baonova.co.jp>
2023-10-03 10:39:11 +0900

    template

---
+++ EngeryCoreDataConvertService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace EngeryCoreDataConvertService
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<SearchClass>(s =>
+                {
+                    s.ConstructUsing(name => new SearchClass());
+                    _ = s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                //Windowsサービスの設定
+                x.RunAsLocalSystem();
+                x.SetDescription("This is EngeryCoreDataConvertService");
+                x.SetDisplayName("aa_EngeryCoreDataConvertService");
+                x.SetServiceName("aa_EngeryCoreDataConvertService");
+            });
+        }
+    }
+}
---
+++ EngeryCoreDataConvertService/SearchClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngeryCoreDataConvertService
+{
+    public class SearchClass : ServiceBase
+    {
+        public void Start()
+        {
+            
+        }
+
+
+
+        public void Stop()
+        {
+        }
+    }
+}
